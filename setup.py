d24cd041bdce43519123b506fde213ab2f6ea984
Georgi Valkov <georgi.t.valkov@gmail.com>
2017-03-01 20:24:06 +0100

    Update classifiers

--- setup.py
+++ setup.py
@@ -10,8 +10,12 @@ classifiers = [
     'Environment :: Console',
     'Topic :: Utilities',
     'Operating System :: OS Independent',
-    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'License :: OSI Approved :: BSD License',
     'Development Status :: 5 - Production/Stable',
 ]
